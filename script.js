86044169bfc267b129fd7b4054e0ce129534351a
ThemiyaL <themiyavliyanage@gmail.com>
2022-06-03 10:38:59 -0700

    changes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function doIt(){
 
     const word1 = document.getElementById("word1");
-
+    const word2 = document.getElementById("word2");
+    const word3 = document.getElementById("word3");
+    const word4 = document.getElementById("word4");
+    const word5 = document.getElementById("word5");
+    const word6 = document.getElementById("word6");
     word1.addEventListener("change", checkWord);
+    word2.addEventListener("change", checkWord);
+    word3.addEventListener("change", checkWord);
+    word4.addEventListener("change", checkWord);
+    word5.addEventListener("change", checkWord);
+    word6.addEventListener("change", checkWord);
 
     var ANSWER = Math.floor(Math.random() * 100)+1;
 
